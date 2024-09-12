51c183d392cdd6ea0bf49f7351605368f2437b80
DanieleBernassola <danielebernassola02@gmail.com>
2024-09-12 16:44:28 +0200

    [UPD] Modified logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,69 +1,41 @@
 <template>
   <header class="py-4 border-b sticky top-0 bg-white">
-    <div
-      class="container mx-auto flex justify-between items-center px-2 sm:px-4"
-    >
+    <div class="container mx-auto flex justify-between items-center px-2 sm:px-4">
       <!-- Logo -->
       <div class="hidden sm:block">
-        <img
-          src="/public/Airbnb_Logo_Bélo.svg.png"
-          alt="Logo"
-          class="max-w-20"
-        />
+        <img src="/boolbnb-logo.png" alt="Logo" class="max-w-20" />
       </div>
 
       <!-- Barra di ricerca -->
       <div class="flex-grow mx-4 flex items-center sm:justify-center">
-        <input
-          type="text"
-          placeholder="Search"
-          class="border px-4 py-2 rounded-full focus:outline-none font-medium shadow sm:w-7/12"
-        />
+        <input type="text" placeholder="Search"
+          class="border px-4 py-2 rounded-full focus:outline-none font-medium shadow sm:w-7/12" />
       </div>
 
       <div class="relative flex items-center gap-2">
         <!-- Icona utente con nome -->
         <p class="hidden sm:block font-semibold" v-if="name">{{ name }}</p>
-        <button
-          @click="toggleMenu"
-          class="focus:outline-none flex items-center space-x-2"
-        >
-          <img
-            src="/public/user-icon.png"
-            alt="User Icon"
-            class="w-8 h-8 rounded-full"
-            style="
+        <button @click="toggleMenu" class="focus:outline-none flex items-center space-x-2">
+          <img src="/public/user-icon.png" alt="User Icon" class="w-8 h-8 rounded-full" style="
               filter: brightness(0) saturate(100%) invert(41%) sepia(7%)
                 saturate(19%) hue-rotate(1deg) brightness(97%) contrast(89%);
-            "
-          />
+            " />
         </button>
 
         <!-- Menu a discesa -->
-        <div
-          v-if="showMenu"
-          class="absolute right-0 mt-48 w-48 bg-white border border-gray-300 rounded-lg shadow-lg"
-        >
+        <div v-if="showMenu" class="absolute right-0 mt-48 w-48 bg-white border border-gray-300 rounded-lg shadow-lg">
           <ul class="py-2">
             <!-- Home sempre visibile -->
             <li>
-              <router-link
-                to="/"
-                class="block px-4 py-2 text-gray-700 hover:bg-gray-100"
-                active-class="text-gray-300"
-              >
+              <router-link to="/" class="block px-4 py-2 text-gray-700 hover:bg-gray-100" active-class="text-gray-300">
                 Home
               </router-link>
             </li>
             <!-- Se non è loggato, mostra Login e Register -->
 
             <li>
-              <a
-                href="http://127.0.0.1:8000/register"
-                target="_blank"
-                class="block px-4 py-2 text-gray-700 hover:bg-gray-100"
-                active-class="text-gray-300"
-              >
+              <a href="http://127.0.0.1:8000/register" target="_blank"
+                class="block px-4 py-2 text-gray-700 hover:bg-gray-100" active-class="text-gray-300">
                 Became An Host
               </a>
             </li>
