5097f11ea5e60547a86db3f6b4c99100beeac3f1
salvatorebriga <salvo.z2302@gmail.com>
2024-09-12 13:16:17 +0200

    removed components

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
-import Login from "../views/Login.vue";
-import Register from "../views/Register.vue";
 
-const routes = [
-  { path: "/", component: Home },
-  { path: "/login", component: Login },
-  { path: "/register", component: Register },
-];
+const routes = [{ path: "/", component: Home }];
 
 const router = createRouter({
   history: createWebHistory(),
