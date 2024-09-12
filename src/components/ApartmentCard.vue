4eabe5faf60ae5f6dedfb8969aa1b13ca27e3a0e
salvatorebriga <salvo.z2302@gmail.com>
2024-09-12 15:34:33 +0200

    fixed apartment card

---
+++ src/components/ApartmentCard.vue
@@ -0,0 +1,43 @@
+<!-- src/components/ApartmentCard.vue -->
+<script>
+export default {
+  props: {
+    apartment: {
+      type: Object,
+      required: true,
+    },
+    imgUrl: {
+      type: String,
+      default: "http://127.0.0.1:8000/storage/",
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="bg-white rounded-lg overflow-hidden">
+    <img
+      v-if="apartment.img"
+      :src="`${imgUrl}${apartment.img}`"
+      alt="Apartment Image"
+      class="w-full h-60 object-cover rounded-lg"
+    />
+    <div class="p-1">
+      <p class="font-semibold text-lg">{{ apartment.title }}</p>
+      <p class="text-gray-600 mt-1">
+        Host: {{ apartment.user.name }} {{ apartment.user.surname }}
+      </p>
+      <p
+        :class="[
+          'mb-4',
+          'mt-1',
+          apartment.is_available ? 'text-green-500' : 'text-red-500',
+        ]"
+      >
+        {{ apartment.is_available ? "Available" : "Not available" }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,14 @@
 <script>
 import axios from "../axios";
+import ApartmentCard from "../components/ApartmentCard.vue";
 
 export default {
+  components: {
+    ApartmentCard,
+  },
   data() {
     return {
       apartments: [],
-      imgUrl: "http://127.0.0.1:8000/storage/",
     };
   },
   created() {
@@ -16,6 +19,7 @@ export default {
       try {
         const response = await axios.get("/apartments");
         this.apartments = response.data;
+        console.log(this.apartments);
       } catch (error) {
         console.error("Error fetching apartments:", error);
       }
@@ -25,32 +29,15 @@ export default {
 </script>
 
 <template>
-  <div class="container py-6 mx-auto">
+  <div class="container py-10 mx-auto">
     <div
       class="grid xs:grid-cols-1 px-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-6"
     >
-      <div
+      <ApartmentCard
         v-for="apartment in apartments"
         :key="apartment.id"
-        class="bg-inherit rounded-lg overflow-hidden"
-      >
-        <img
-          v-if="apartment.img"
-          :src="`${imgUrl}${apartment.img}`"
-          alt="Apartment Image"
-          class="w-full h-60 object-cover rounded-lg"
-        />
-
-        <div class="p-1">
-          <p class="font-semibold mt-2">{{ apartment.title }}</p>
-          <p class="text-gray-600 mt-1">
-            Host: {{ apartment.user.name }} {{ apartment.user.surname }}
-          </p>
-          <p class="mb-4 mt-1">
-            {{ apartment.is_available ? "Available" : "Not available" }}
-          </p>
-        </div>
-      </div>
+        :apartment="apartment"
+      />
     </div>
   </div>
 </template>
