bbd4b2755f4e28ed9844638f57cc5cef2e23103a
DanieleBernassola <danielebernassola02@gmail.com>
2024-09-17 17:10:54 +0200

    [UPD] Added details page route

--- src/router/index.js
+++ src/router/index.js
@@ -4,28 +4,28 @@ import AdvancedSearch from "../views/AdvancedSearch.vue";
 import DetailsApartment from "../views/DetailsApartment.vue";
 
 const routes = [
-	{
-		path: "/",
-		name: "home",
-		component: Home,
-	},
-	{
-		path: "/advanced-search",
-		name: "advanced-search",
-		component: AdvancedSearch,
-		props: (route) => ({ query: route.query.query }),
-	},
-	{
-		path: "/apartment/:title&id=:id",
-		name: "details-apartment",
-		component: DetailsApartment,
-		props: true,
-	},
+  {
+    path: "/",
+    name: "home",
+    component: Home,
+  },
+  {
+    path: "/advanced-search",
+    name: "advanced-search",
+    component: AdvancedSearch,
+    props: (route) => ({ query: route.query.query }),
+  },
+  {
+    path: "/apartment/:id",
+    name: "DetailsApartment",
+    component: DetailsApartment,
+    props: true,
+  },
 ];
 
 const router = createRouter({
-	history: createWebHistory(),
-	routes,
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
