718d1ce5b578c500e263f0141090fc32304e7a01
DanieleBernassola <danielebernassola02@gmail.com>
2024-09-20 13:14:26 +0200

    [UPD] Added views(to fix)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,38 +1,20 @@
 <template>
   <main>
-    <div
-      class="dark:text-white py-10 px-6 transition-all duration-300 3xl:px-20"
-    >
+    <div class="dark:text-white py-10 px-6 transition-all duration-300 3xl:px-20">
       <div
-        class="gap-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-6 transition-all duration-300"
-      >
-        <ApartmentCard
-          v-for="apartment in sponsoredApartments"
-          @click="showApartmentDetails(apartment.id)"
-          :title="apartment.title"
-          :img="apartment.img"
-          :host="`${apartment.user.name} ${apartment.user.surname}`"
-          :isAvailable="apartment.is_available"
-          :key="apartment.id"
-        />
+        class="gap-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-6 transition-all duration-300">
+        <ApartmentCard v-for="apartment in sponsoredApartments" @click="showApartmentDetails(apartment.id)"
+          :title="apartment.title" :img="apartment.img" :host="`${apartment.user.name} ${apartment.user.surname}`"
+          :isAvailable="apartment.is_available" :key="apartment.id" />
       </div>
-      <h2
-        class="pt-6 pb-12 font-bold text-2xl sm:text-4xl transition-all duration-300"
-      >
+      <h2 class="pt-6 pb-12 font-bold text-2xl sm:text-4xl transition-all duration-300">
         Selected for you
       </h2>
       <div
-        class="gap-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-6 transition-all duration-300"
-      >
-        <ApartmentCard
-          v-for="apartment in normalApartments"
-          @click="showApartmentDetails(apartment.id)"
-          :title="apartment.title"
-          :img="apartment.img"
-          :host="`${apartment.user.name} ${apartment.user.surname}`"
-          :isAvailable="apartment.is_available"
-          :key="apartment.id"
-        />
+        class="gap-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-6 transition-all duration-300">
+        <ApartmentCard v-for="apartment in normalApartments" @click="showApartmentDetails(apartment.id)"
+          :title="apartment.title" :img="apartment.img" :host="`${apartment.user.name} ${apartment.user.surname}`"
+          :isAvailable="apartment.is_available" :key="apartment.id" />
       </div>
     </div>
   </main>
@@ -82,9 +64,18 @@ export default {
         [array[i], array[j]] = [array[j], array[i]];
       }
     },
-    showApartmentDetails(id) {
-      this.$router.push({ name: "details-apartment", params: { id } });
-    },
+    async showApartmentDetails(id) {
+      try {
+        // Registra la visualizzazione tramite API
+        await axios.post(`/apartments/${id}`);
+
+        // Vai ai dettagli dell'appartamento
+        this.$router.push({ name: "details-apartment", params: { id } });
+      } catch (error) {
+        console.error("Error registering view:", error);
+      }
+    }
+
   },
 };
 </script>
